4c3140451833c23a4826e21a09d9ce3b524518e0
FacuRoffet99 <facundo.roffet@cs.uns.edu.ar>
2025-08-09 04:38:50 -0300

    I'm tired

--- .github/workflows/promote.py
+++
@@ -1,8 +0,0 @@
-import os
-from wandb import promote_run_by_id
-
-assert os.environ['WANDB_API_KEY'], 'You must set the WANDB_API_KEY environment variable'
-
-if __name__ == "__main__":
-    entity, project, collection = 'FacuRoffet99', 'pytorch-intro', 'MNIST Classifier'
-    promote_run_by_id(entity, project, collection, os.environ['RUN_ID'], os.environ['REGISTRY_TAG'])
